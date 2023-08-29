2306a1dbbd48c99c46bf7203532176d3abfa2aa2
PjHurtig <>
2023-08-29 15:26:31 +0000

    add filters and post count to profile views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,6 +5,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,5 +15,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'posts_count',
         ]
