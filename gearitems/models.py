e04170c3a900a73e937a5d02f500da75a70473cb
PjHurtig <>
2023-09-01 14:49:01 +0000

    add gear item model

--- gearitems/models.py
+++ gearitems/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from gearlists.models import GearList
 
-# Create your models here.
+
+class GearItem(models.Model):
+    """
+    Gear items, related to user and gearlist
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    gearlist = models.ForeignKey(GearList, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+    image = models.ImageField(
+        upload_to='images/', default='../gear_gw6qtj.jpg', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
