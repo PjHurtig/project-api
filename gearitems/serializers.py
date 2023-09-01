9ba6853c872ec7329c55a367b8f2105ba9869977
PjHurtig <>
2023-09-01 14:58:02 +0000

    add gearitem serializer

---
+++ gearitems/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import GearItem
+
+
+class GearItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the GearItem model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = GearItem
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'gearlist', 'created_at', 'updated_at', 'name', 'about', 'image',
+        ]
+
+
+class GearItemDetailSerializer(GearItemSerializer):
+    """
+    Serializer for the GearItem model used in Detail view
+    """
+    gearlist = serializers.ReadOnlyField(source='gearlist.id')
